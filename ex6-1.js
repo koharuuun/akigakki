fa16b71717a2d5f2b4b15a10c77e5d9f76c99bca
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-10-22 09:49:54 +0900

    Update ex6-1.js

--- ex6-1.js
+++ ex6-1.js
@@ -7,4 +7,5 @@ function sayhello61() {
 var who65 = 'world'; function sayhello65() { alert('Hello, '+ who65 +'!') }
 function taro65() { who65 ='太郎';
                    function hanako65() { who65='花子'; }
-                   
+function taro65() { alert('太郎が来た'); }
+                   function hanako65() { alert('花子が来た'); }
