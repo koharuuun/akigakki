c3447db99ab9084f905a0f2b81a5a3063ae83843
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-11-01 09:40:32 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -1,4 +1,4 @@
-function sayhello68() { alert('Hello,'+ document.getElementByld('who68').innnerHTML +'!'); }
+function sayhello68() { alert('Hello,' + document.getElementByld('who68').innerHTML +'!'); }
 function taro68() { document.getElementByld('who68').innnerHTML='太郎'; }
 function hanako68() { document.getElementByld('who68').innnerHTML='花子'; }
 
