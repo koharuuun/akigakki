49ab8ba42dd5f8e9c41735dd400f99b8ec585c01
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-11-12 10:17:36 +0900

    Update ex06-88.js

--- ex06-88.js
+++ ex06-88.js
@@ -1,4 +1,4 @@
 function gazo688()
 {document.getElementByld('who688').innerHTML=
-  '<img src="./image/masukotto_jokoma002" alt="オン">'
+  '<img src="./image/masukotto_jokoma002.heic" alt="オン">'
   ;}
