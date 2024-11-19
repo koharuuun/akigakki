55e81cd95b73ffdb62b220b68367480e0403c68d
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-11-19 09:43:30 +0900

    Create ex06-10.js

---
+++ ex06-10.js
@@ -0,0 +1,8 @@
+function gazo610()
+{document.getElementById('who610').innerHTML=
+'<img src="./image/josaisports.jpg" alt="オン">'
+  ;}
+function gazo6101()
+{document.getElementById('who6101').innerHTML=
+'<img src="./image/JU.jpg" alt="オン">'
+  ;}
