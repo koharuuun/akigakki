5ee19379bc59b22892d3594e67bc7d2d0e5472a7
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-11-19 09:49:37 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,8 +1,8 @@
 function gazo610()
 {document.getElementById('who610').innerHTML=
-'<img src="./image/josaisports.jpg" alt="オン">'
+'<img src="./image/josaisports.png" alt="オン">'
   ;}
 function gazo6101()
 {document.getElementById('who6101').innerHTML=
-'<img src="./image/JU.png" alt="オン">'
+'<img src="./image/JU.jpg" alt="オン">'
   ;}
