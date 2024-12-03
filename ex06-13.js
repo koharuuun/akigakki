22ef66c5e419839c14cd8b316d9e0976bd36b38e
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-03 09:26:55 +0900

    Update ex06-13.js

--- ex06-13.js
+++ ex06-13.js
@@ -1,11 +1,11 @@
-...
 function mes(){ 
-  alert("３秒経ちました！"); }
+  alert("３秒経ちました！");
+}
 function hyoji13(){ 
   //表示する文字　
   var str13="一文字ずつ表します。";
 //テキストボックスの文字数 
-  var cnt13=document.timer13.moji13.value.length;
+  var cnt13= document.timer13.moji13.value.length;
 //文字が全部表示されているか確認
 if ( cnt13 < 11 ){ 
   //現在より１文字多く切り出して表示
