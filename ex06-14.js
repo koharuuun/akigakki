34cbe5344eecb1a276eccae4b435f9d0ade20530
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-03 12:51:41 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -2,3 +2,7 @@ function hyoji15() {
   document.getElementById('who2').innerHTML=
 '<img src="./image/sunupii.jpg" alt="オン">'
   ;}
+function mes() { 
+  document.getElementById('who2').innerHTML=
+'<img src="./image/sunupi.png" alt="オン">'
+  ;}
