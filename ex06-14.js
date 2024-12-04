4bedc5c8c00032086b46d58420a017c3962a766e
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-05 07:12:17 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,3 +1,7 @@
 function gazo15() {
   document.getElementById('who2').src=
-'./image/sunupii.jpg'; }
+'./image/sunupii.jpg';
+setTimeout(}
+function gazo16() {
+  document.getElementById('who2').src=
+'./image/sunupi.png'; }
