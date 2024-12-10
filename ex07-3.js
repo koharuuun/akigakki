ae87bdb869c2776ef48bfbbf1a3c0e19b6f412ab
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-10 10:28:26 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,4 +1,4 @@
 function adds() {
   var h = document.getElementById('sintyo').value;
   var w =document.getElementById('taijyu').value;
-  alert( Number(h)*Number(h)/Number(w)); }
+  alert( Number(w)/Number(h)*Number(h)); }
