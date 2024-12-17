bb5968254ed00c87713f6fc5ec7f88ba30914317
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-17 10:23:00 +0900

    Create ex7-11.js

---
+++ ex7-11.js
@@ -0,0 +1,9 @@
+function nazo711() {
+  if (document.getElementById('answer711').value=='% 4==0'{
+    alert('閏年'); }
+  else if(document.getElementById('answer711').value=='ゆめ'){
+    alert('閏年'); }
+   else if(document.getElementById('answer711').value!=='夢'){
+    alert('平年'); }
+   else if(document.getElementById('answer711').value!=='ゆめ'){
+        alert('平年'); } }
