f2695ba2f7a878e8c2712c26fee1f0a00629e395
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-17 10:08:14 +0900

    Update ex7-10.js

--- ex7-10.js
+++ ex7-10.js
@@ -4,10 +4,10 @@ function nazo710() {
   alert( Number(w2)/(Number(h2)*Number(h2))); }
 
 function nazo710() {
-  if (document.getElementById('answer710').value<='18.5'){
+  if (document.getElementById('answer710').value<=Number('18.5') ){
     alert('低体重'); }
-  else if(document.getElementById('answer79').value<='18.5'>'25'){
+  else if(document.getElementById('answer79').value>=Number('18.5')<Number('25') ){
     alert('普通'); }
-   else if(document.getElementById('answer79').value>='25'){
-    alert('肥満'); }
+   else if(document.getElementById('answer79').value>=Number('25') ){
+    alert('肥満'); } }
    
