6d3d3baa24222ac58c769f4f1f4152d5a703a6b5
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-17 10:25:36 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,9 +1,5 @@
+
 function nazo711() {
-  if (document.getElementById('answer711').value=='% 4==0'{
+  if (document.getElementById('answer711').value<=Number('%4==0') ){
     alert('閏年'); }
-  else if(document.getElementById('answer711').value=='ゆめ'){
-    alert('閏年'); }
-   else if(document.getElementById('answer711').value!=='夢'){
-    alert('平年'); }
-   else if(document.getElementById('answer711').value!=='ゆめ'){
-        alert('平年'); } }
+ 
