0fecda9ab9b09d24c7aa57e569ece64f6cf6f36a
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-17 10:34:21 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -2,4 +2,9 @@
 function nazo711() {
   if (document.getElementById('answer711').value<=Number('%4==0') ){
     alert('閏年'); }
- 
+else if(document.getElementById('answer711').value>=Number('%400==0') ){
+    alert('閏年'); }
+   else if(document.getElementById('answer79').value>=Number('%4!==o') ){
+    alert('平年'); } 
+  else if(document.getElementById('answer79').value>=Number('%400!==o') ){
+    alert('平年'); } }
