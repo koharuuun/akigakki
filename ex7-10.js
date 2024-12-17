0db4aa0b67683de96163d919af33c157c727c63f
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-17 09:57:04 +0900

    Update ex7-10.js

--- ex7-10.js
+++ ex7-10.js
@@ -3,3 +3,12 @@ function nazo10() {
   var w2 =document.getElementById('taijyu2').value;
   alert( Number(w2)/(Number(h2)*Number(h2))); }
 
+function nazo10() {
+  if (document.getElementById('answer79').value=='夢'){
+    alert('あたり！'); }
+  else if(document.getElementById('answer79').value=='ゆめ'){
+    alert('あたり！'); }
+   else if(document.getElementById('answer79').value!=='夢'){
+    alert('はずれ'); }
+   else if(document.getElementById('answer79').value!=='ゆめ'){
+        alert('はずれ'); } }
