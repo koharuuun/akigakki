25745707976f0c5e30de81614e22a87adb88516d
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-20 10:15:37 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -2,7 +2,7 @@ function nazo710() {
   var h2 = document.getElementById('sintyo2').value;
   var w2 =document.getElementById('taijyu2').value;
   alert( Number(w2)/(Number(h2)*Number(h2)));
-  if (<=Number('18.5') ){
+  if (<=Number('18.5')){
     alert('低体重'); }
   else if( >=Number('18.5')<Number('25') ){
     alert('普通'); }
