08df999a60b77567154024dc1e27523a908b6d27
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-20 10:30:26 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -2,10 +2,10 @@ function nazo710() {
   var h2 = document.getElementById('sintyo2').value;
   var w2 =document.getElementById('taijyu2').value;
   alert( Number(w2)/(Number(h2)*Number(h2)));
-  if (<=Number('18.5')){
+  if ((Number(w2)/(Number(h2)*Number(h2)))<=Number('18.5')){
     alert('低体重'); }
-  else if( >=Number('18.5')<Number('25') ){
+  else if( Number('18.5')<=(Number(w2)/(Number(h2)*Number(h2)))<Number('25')){
     alert('普通'); }
-  else if( >=Number('25')){
+  else if( (Number(w2)/(Number(h2)*Number(h2)))>=Number('25')){
       alert('肥満'); } }
   
