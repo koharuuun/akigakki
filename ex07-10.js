b64aee8b9fe8cc5ec4019c8c175c6d4cd4ff2d8c
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-21 06:56:36 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -4,7 +4,7 @@ function nazo710() {
   alert( Number(w2)/(Number(h2)*Number(h2)));
   if ((Number(w2)/(Number(h2)*Number(h2)))<=Number('18.5')){
     alert('低体重'); }
-  else if( Number('18.5')<=(Number(w2)/(Number(h2)*Number(h2)))<Number('25')){
+  else if( Number('18.5')<=(Number(w2)/(Number(h2)*Number(h2)))&&(Number(w2)/(Number(h2)*Number(h2)))<Number('25')){
     alert('普通'); }
   else if( Number('25')<=(Number(w2)/(Number(h2)*Number(h2)))){
       alert('肥満'); } }
