887a90ca17a07a1d8a8d5ceef4319f1660caffa3
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-21 07:11:03 +0900

    Update ex07-11.js

--- ex07-11.js
+++ ex07-11.js
@@ -1,11 +1,11 @@
 
 function nazo711() {
-  if((Number(document.getElementById('answer711').value)==('%4==0'))){
+  if((Number(document.getElementById('answer711').value)%4)==Number('0')){
     alert('閏年'); }
-else if((Number(document.getElementById('answer711').value==('%400==0'))){
+else if((Number(document.getElementById('answer711').value)%400)==Number('0'))){
     alert('閏年'); }
-   else if((Number(document.getElementById('answer711').value==('%4!==0'))){
+   else if((Number(document.getElementById('answer711').value)%4)!==Number('0'))){
     alert('平年'); } 
-  else if((Number(document.getElementById('answer711').value==('%400!==0'))){
+  else if((Number(document.getElementById('answer711').value)%400)!==Number('0'))){
     alert('平年'); } }
  
