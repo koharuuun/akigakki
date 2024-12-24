7633065d08a75baec593d5507c309938d2bc37db
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-24 09:20:45 +0900

    Update ex07-11.js

--- ex07-11.js
+++ ex07-11.js
@@ -4,9 +4,6 @@ function nazo711() {
     alert('閏年'); }
   else if((Number(no1)%400)==Number('0')){
     alert('閏年'); }
-   else if((Number(no1)%4)!==Number('0')){
-    alert('平年'); }
-   else if((Number(no1)%400)!==Number('0')){
-    alert('平年'); }
 else if((Number(no1)%100)==Number('0')){
-    alert('平年'); }}
+    alert('平年'); }
+else{alert('平年')}}
