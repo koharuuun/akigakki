85cc22a8f011c3c2275e96e295ccef2375e0f878
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-24 09:01:03 +0900

    Update ex07-11.js

--- ex07-11.js
+++ ex07-11.js
@@ -1,11 +1,10 @@
-
 function nazo711() {
-  if((Number(document.getElementById('answer711').value)%4)==Number('0')){
+  var no1 = document.getElementById('answer711').value;
+  if ((Number(no1)%4)==Number('0')){
     alert('閏年'); }
-else if((Number(document.getElementById('answer711').value)%400)==Number('0'))){
+  else if((Number(no1)%400)==Number('0')){
     alert('閏年'); }
-   else if((Number(document.getElementById('answer711').value)%4)!==Number('0'))){
-    alert('平年'); } 
-  else if((Number(document.getElementById('answer711').value)%400)!==Number('0'))){
-    alert('平年'); } }
- 
+   else if((Number(no1)%4)!==Number('0')){
+    alert('平年'); }
+   else if((Number(no1)%400)!==Number('0')){
+    alert('平年'); }}
